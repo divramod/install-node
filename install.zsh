e3d4dd67ca1b257b0ec69a039ffe00ce9804a936
divramod <divramod@gmail.com>
2015-05-12 16:01:42 +0200

    updated to install zsh

--- install.zsh
+++ install.zsh
@@ -1,5 +1,5 @@
 echo "hello"
-apt-get install zsh
-apt-get install git-core
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-chsh -s `which zsh`
+sudo apt-get install zsh
+sudo apt-get install git-core
+sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+sudo chsh -s `which zsh`
